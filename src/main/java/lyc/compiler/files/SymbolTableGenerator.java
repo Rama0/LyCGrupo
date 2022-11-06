d8a84305bb455c4c56df08724c2730e770d65056
ezeq8 <ezequiel.silvero@hotmail.com>
2022-11-06 13:33:33 -0300

    1-Se agregaron casos de prueba al archivo test.txt 2-Se implemento la carga a la tabla de simbolos y la oportuna escritura del archivo symbols-table.txt

--- src/main/java/lyc/compiler/files/SymbolTableGenerator.java
+++ src/main/java/lyc/compiler/files/SymbolTableGenerator.java
@@ -2,11 +2,87 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SymbolTableGenerator implements FileGenerator{
 
+
+    private static final ArrayList<String> listLexer = new ArrayList<>();
+    private static final ArrayList<SymbolTable> ts = new ArrayList<>();
+    private static final ArrayList<SymbolTable> lsInteger = new ArrayList<>();
+    private static final ArrayList<SymbolTable> lsFloat = new ArrayList<>();
+    private static final ArrayList<SymbolTable> lsString = new ArrayList<>();
+    private static final ArrayList<SymbolTable> lsCte = new ArrayList<>();
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        fileWriter.write("NOMBRE TIPODATO VALOR LONGITUD\n");
+        for(SymbolTable e : lsInteger)
+        {
+            fileWriter.write(e.getName() + " "+ e.getDataType()+"   \n");
+        }
+        for(SymbolTable e : lsFloat )
+        {
+            fileWriter.write(e.getName() + " "+ e.getDataType()+"   \n");
+        }
+        for(SymbolTable e : lsString)
+        {
+            fileWriter.write(e.getName() + " "+ e.getDataType()+"  \n");
+        }
+
+        for(SymbolTable e : lsCte)
+        {
+            if(e.getName().startsWith("\""))
+            {
+                e.setValue(e.getName().substring(1,e.getName().length()-1));
+                e.setName("_"+e.getValue());
+                e.setLength(e.getValue().length());
+
+            }
+            else
+            {
+                e.setValue(e.getName());
+                e.setLength(e.getName().length());
+                e.setName("_"+e.getName());
+            }
+
+            fileWriter.write(e.getName()+"       "+e.getValue()+" "+e.getLength()+"\n");
+        }
+        //fileWriter.write("TODO");
+    }
+
+    public ArrayList<String> getListLexer(){
+        return listLexer;
+    }
+
+    public ArrayList<SymbolTable> getLsCte(){
+        return lsCte;
+    }
+
+    public ArrayList<SymbolTable> getTs(){
+        return ts;
+    }
+
+    public  void hello(String name){
+        System.out.println("Mi Identificador es "+name);
+    }
+
+
+    public void asignarTipodeDato(int typeDato){
+        for (SymbolTable elem: ts) {
+            if(typeDato == 0)
+            {
+                elem.setDataType("Int");
+                lsInteger.add(elem);
+            } else if (typeDato == 1 ) {
+
+                elem.setDataType("Float");
+                lsFloat.add(elem);
+            }
+            else {
+                elem.setDataType("String");
+                lsString.add(elem);
+            }
+        }
+        ts.clear();
     }
 }
