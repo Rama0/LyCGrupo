235308bf2471388dbcee7da71283a8a2390a9804
ezeq8 <ezequiel.silvero@hotmail.com>
2022-11-06 17:01:13 -0300

    Se agrego logica para escribir la notacion polaca inversa en expresiones, asignaciones y la funcion iguales

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -2,11 +2,23 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class IntermediateCodeGenerator implements FileGenerator {
 
+    private static final ArrayList<String> polaca = new ArrayList<>();
+    private static final Stack<String> pila = new Stack<>();
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        for (String e: polaca)
+        {
+            fileWriter.write(e+" ");
+        }
+        //fileWriter.write("TODO");
+    }
+
+    public ArrayList<String> getPolaca (){
+        return polaca;
     }
 }
