2eb8b94849ed76bf5d83c81e11bb85c0ce54e1d6
IvanSanchez1 <isanchezvedelago@alumno.unlam.edu.ar>
2022-11-20 11:44:42 -0300

    reglas para para iteracion

--- src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
+++ src/main/java/lyc/compiler/files/IntermediateCodeGenerator.java
@@ -9,15 +9,24 @@ public class IntermediateCodeGenerator implements FileGenerator {
 
     private static final ArrayList<String> polaca = new ArrayList<>();
     private static final Stack<String> pila = new Stack<>();
+
+    private static final Stack<Integer> pilaInt = new Stack<>();
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        for (String e: polaca)
+        for (int i = 0; i < polaca.size(); i ++)
         {
-            fileWriter.write(e+" ");
+            fileWriter.write(i + "- " + polaca.get(i) + "\n");
         }
-        //fileWriter.write("TODO");
     }
     public ArrayList<String> getPolaca (){
         return polaca;
+    }
+
+    public Stack<String> getPila (){
+        return pila;
+    }
+
+    public Stack<Integer> getPilaInt (){
+        return pilaInt;
     }
 }
