36c9238bd53a30b5b17cd9f39dbb99fc81eab041
ezeq8 <ezequiel.silvero@hotmail.com>
2022-11-25 19:58:18 -0300

    1-Agrego logica para comenzar la generacion de codigo assembler

--- src/main/java/lyc/compiler/files/AsmCodeGenerator.java
+++ src/main/java/lyc/compiler/files/AsmCodeGenerator.java
@@ -2,11 +2,19 @@ package lyc.compiler.files;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class AsmCodeGenerator implements FileGenerator {
 
+    private static final Stack<String> pila = new Stack<>();
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
         fileWriter.write("TODO");
+    }
+
+    public void generar_assembler(ArrayList<String> polaca)
+    {
+        
     }
 }
